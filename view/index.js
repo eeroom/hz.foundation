be1d7916a54535a07dee7eb74eb1cdb4a8a8a507
Deroomor <1344206656@qq.com>
2019-07-16 22:07:14 +0800

    引入ant和ant-mobile

--- view/index.js
+++ view/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import { createStore } from 'redux';
 import { Provider } from 'react-redux'
 import Controller from '../controller/Controller';
+import {  Route,HashRouter } from 'react-router-dom';
 
 let store = createStore((state, action) => {
   // console.log("state", state);
@@ -16,6 +17,26 @@ let store = createStore((state, action) => {
 Controller.dispatch=store.dispatch;
 Controller.getState=store.getState;
 
+let lstView=require.context("./view", true, /\.js$/);
+let lstComponent= lstView.keys().map(key => ({
+  path:lstView.resolve(key).toLowerCase()
+  ,component:lstView(key).default
+}));
+
+let View404=()=>{
+  return (<div>页面跑了( ▼-▼ )</div>);
+}
+
 ReactDOM.render(<Provider store={store}>
-  <MyPageWrapper msg={"hello world"}></MyPageWrapper>
+   <HashRouter>
+     
+    <Route exact path="/:controller/:action" render={x=>{
+                console.log("match",x)
+                let url= x.match.url.toLowerCase();
+                let View=(lstComponent.find(x=>x.path.indexOf(url)>=0) ||{}).component;
+                console.log("View",View)
+                View=View||View404;
+                return (<View {...x} />);
+            }}></Route>
+    </HashRouter>
 </Provider>,document.getElementById("root"))
