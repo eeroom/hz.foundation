1b635f497b67a2ce30c82089d920758e0522c1ae
邓常青 <eedroom@163.com>
2019-07-17 14:36:20 +0800

    认证拦截

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom'
 import { createStore } from 'redux';
 import { Provider } from 'react-redux'
 import Controller from './controller/Controller';
-import {  Route,HashRouter } from 'react-router-dom';
+import {  Route,HashRouter,Redirect } from 'react-router-dom';
+import IndexMobile from './view/home/indexMobile'
+import Login from './view/account/login'
 
 let store = createStore((state, action) => {
   // console.log("state", state);
@@ -28,15 +30,28 @@ let View404=()=>{
   return (<div>页面跑了( ▼-▼ )</div>);
 }
 
+const Authenrization = ({view:View,...props}) => {
+  if (!window.userInfo || !window.userInfo.name)
+    return (<Redirect to={"/account/login"} {...props}></Redirect>)
+  return <View {...props} />;
+}
+
+
 ReactDOM.render(<Provider store={store}>
    <HashRouter>
+   <Route  exact path="/account/login" render={x=>{
+                 return (<Login {...x} />);
+            }} ></Route>
+   <Route  exact path="/" render={x=>{
+                 return (<Authenrization view={IndexMobile} {...x} />);
+            }} ></Route>
     <Route exact path="/:controller/:action" render={x=>{
                 console.log("match",x)
                 let url= x.match.url.toLowerCase();
                 let View=(lstComponent.find(x=>x.path.indexOf(url)>=0) ||{}).component;
                 console.log("View",View)
                 View=View||View404;
-                return (<View {...x} />);
+                return (<Authenrization view={View} {...x} />);
             }}></Route>
     </HashRouter>
 </Provider>,document.getElementById("root"))
