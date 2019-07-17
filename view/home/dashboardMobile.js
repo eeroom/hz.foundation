57f336bc4a2f71a260c0c379b0b42d65430908b5
邓常青 <eedroom@163.com>
2019-07-17 12:02:20 +0800

    增加移动端主页面

---
+++ view/home/dashboardMobile.js
@@ -0,0 +1,194 @@
+import React from 'react';
+import { TabBar } from 'antd-mobile';
+import { Grid } from 'antd-mobile';
+import { NavBar, Icon } from 'antd-mobile';
+class DashboardMobile extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedTab: 'redTab',
+            hidden: false,
+            fullScreen: true,
+        };
+    }
+
+
+    renderContent(pageText) {
+        const {history}=this.props;
+        console.log("props",this.props)
+        const data = Array.from(new Array(9)).map((_val, i) => ({
+            icon: 'https://gw.alipayobjects.com/zos/rmsportal/nywPmnTAvTmLusPxHPSu.png',
+            text: `name${i}`,
+        }));
+
+        const data1 = Array.from(new Array(9)).map(() => ({
+            icon: 'https://gw.alipayobjects.com/zos/rmsportal/WXoqXTHrSnRcUwEaQgXJ.png',
+        }));
+        data[0].text="创建检查表"
+        data[1].text="修改检查表"
+        data[2].text="分配检查人"
+
+        // console.log("data",data);
+        return (
+            <div style={{ backgroundColor: 'white', height: '100%',  }}>
+                {/* <div style={{ paddingTop: 60 }}>Clicked “{pageText}” tab， show “{pageText}” information</div>
+                <a style={{ display: 'block', marginTop: 40, marginBottom: 20, color: '#108ee9' }}
+                    onClick={(e) => {
+                        e.preventDefault();
+                        this.setState({
+                            hidden: !this.state.hidden,
+                        });
+                    }}
+                >
+                    Click to show/hide tab-bar
+        </a>
+                <a style={{ display: 'block', marginBottom: 600, color: '#108ee9' }}
+                    onClick={(e) => {
+                        e.preventDefault();
+                        this.setState({
+                            fullScreen: !this.state.fullScreen,
+                        });
+                    }}
+                >
+                    Click to switch fullscreen
+        </a> */}
+         <NavBar
+      mode="light"
+      icon={<Icon type="left" />}
+      onLeftClick={() => history.goBack()}
+      rightContent={[
+        <Icon key="0" type="search" style={{ marginRight: '16px' }} />,
+        <Icon key="1" type="ellipsis" />,
+      ]}
+    >现场检查</NavBar>
+                <div className="sub-title">准备</div>
+                <Grid data={data} columnNum={3}  />
+
+                <div className="sub-title">本次检查</div>
+                <Grid data={data} columnNum={3} />
+
+
+            </div>
+        );
+    }
+
+    render() {
+        return (
+
+     <div style={this.state.fullScreen ? { position: 'fixed', height: '100%', width: '100%', top: 0 } : { height: 400 }}>
+                <TabBar
+                    unselectedTintColor="#949494"
+                    tintColor="#33A3F4"
+                    barTintColor="white"
+                    hidden={this.state.hidden}
+                >
+                    <TabBar.Item
+                        title="检查"
+                        key="Life"
+                        icon={<div style={{
+                            width: '22px',
+                            height: '22px',
+                            background: 'url(https://zos.alipayobjects.com/rmsportal/sifuoDUQdAFKAVcFGROC.svg) center center /  21px 21px no-repeat'
+                        }}
+                        />
+                        }
+                        selectedIcon={<div style={{
+                            width: '22px',
+                            height: '22px',
+                            background: 'url(https://zos.alipayobjects.com/rmsportal/iSrlOTqrKddqbOmlvUfq.svg) center center /  21px 21px no-repeat'
+                        }}
+                        />
+                        }
+                        selected={this.state.selectedTab === 'blueTab'}
+                        badge={1}
+                        onPress={() => {
+                            this.setState({
+                                selectedTab: 'blueTab',
+                            });
+                        }}
+                        data-seed="logId"
+                    >
+                        {this.renderContent('Life')}
+                    </TabBar.Item>
+                    <TabBar.Item
+                        icon={
+                            <div style={{
+                                width: '22px',
+                                height: '22px',
+                                background: 'url(https://gw.alipayobjects.com/zos/rmsportal/BTSsmHkPsQSPTktcXyTV.svg) center center /  21px 21px no-repeat'
+                            }}
+                            />
+                        }
+                        selectedIcon={
+                            <div style={{
+                                width: '22px',
+                                height: '22px',
+                                background: 'url(https://gw.alipayobjects.com/zos/rmsportal/ekLecvKBnRazVLXbWOnE.svg) center center /  21px 21px no-repeat'
+                            }}
+                            />
+                        }
+                        title="查询"
+                        key="Koubei"
+                        badge={'new'}
+                        selected={this.state.selectedTab === 'redTab'}
+                        onPress={() => {
+                            this.setState({
+                                selectedTab: 'redTab',
+                            });
+                        }}
+                        data-seed="logId1"
+                    >
+                        {this.renderContent('Koubei')}
+                    </TabBar.Item>
+                    <TabBar.Item
+                        icon={
+                            <div style={{
+                                width: '22px',
+                                height: '22px',
+                                background: 'url(https://zos.alipayobjects.com/rmsportal/psUFoAMjkCcjqtUCNPxB.svg) center center /  21px 21px no-repeat'
+                            }}
+                            />
+                        }
+                        selectedIcon={
+                            <div style={{
+                                width: '22px',
+                                height: '22px',
+                                background: 'url(https://zos.alipayobjects.com/rmsportal/IIRLrXXrFAhXVdhMWgUI.svg) center center /  21px 21px no-repeat'
+                            }}
+                            />
+                        }
+                        title="分析"
+                        key="Friend"
+                        dot
+                        selected={this.state.selectedTab === 'greenTab'}
+                        onPress={() => {
+                            this.setState({
+                                selectedTab: 'greenTab',
+                            });
+                        }}
+                    >
+                        {this.renderContent('Friend')}
+                    </TabBar.Item>
+                    <TabBar.Item
+                        icon={{ uri: 'https://zos.alipayobjects.com/rmsportal/asJMfBrNqpMMlVpeInPQ.svg' }}
+                        selectedIcon={{ uri: 'https://zos.alipayobjects.com/rmsportal/gjpzzcrPMkhfEqgbYvmN.svg' }}
+                        title="我"
+                        key="my"
+                        selected={this.state.selectedTab === 'yellowTab'}
+                        onPress={() => {
+                            this.setState({
+                                selectedTab: 'yellowTab',
+                            });
+                        }}
+                    >
+                        {this.renderContent('My')}
+                    </TabBar.Item>
+                </TabBar>
+            </div>
+        
+
+           );
+    }
+}
+
+export default DashboardMobile
