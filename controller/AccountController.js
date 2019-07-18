7647afb2f14a0dcece5331800137c57e0a2b3421
邓常青 <eedroom@163.com>
2019-07-18 15:09:14 +0800

    场景：1，2任务没有依赖，3依赖1和2，用promise解决

--- controller/AccountController.js
+++ controller/AccountController.js
@@ -11,14 +11,29 @@ class  AccountController extends Controller{
 
     async login(parameter){
 
-        new Promise((ok,notok)=>notok(4)).then(x=>console.log("promiseThen",x))
-        .catch(x=>console.log("promisCatch",x));
+        // new Promise((ok,notok)=>notok(4)).then(x=>console.log("promiseThen",x))
+        // .catch(x=>console.log("promisCatch",x));
 
         console.log("parameter",parameter)
-        let rt=await httpclient.post(api.Account.Login,parameter);
-        console.log("rt",rt);
-        httpclient.post(api.Account.Login,parameter)
-        .then(x=>console.log("rt",x));
+        // let rt=await httpclient.post(api.Account.Login,parameter);
+        // console.log("rt",rt);
+        let rtWrapper= await new Promise((ok,notok)=>{
+            let value1,value2;
+            httpclient.post(api.Account.Login,parameter)
+            .then(x=>{
+                value1=x;
+                value1&&value2&&ok({value1,value2})
+            })
+            httpclient.post(api.Account.Login,parameter)
+            .then(x=>{
+                value2=x;
+                value1&&value2&&ok({value1,value2})
+            })
+        });
+        console.log("rtWrapper",rtWrapper);
+
+        // httpclient.post(api.Account.Login,parameter)
+        // .then(x=>console.log("rt",x));
     }
 }
 
--- utils/httpclient.js
+++ utils/httpclient.js
@@ -1,11 +1,10 @@
 import axios from 'axios'
-const host="http://localhost:58781"
-const httpcleint={
-    get(url,parameter){
-        return axios.get(host+url,parameter).catch(x=>console.log("catch",x));
+const host = "http://localhost:58781"
+const httpcleint = {
+    get(url, parameter) {
+        return axios.get(host + url, parameter).catch(x => console.log("catch", x));
     },
-    post(url,parameter){
-        //new Promise(axios.post(host+url,parameter).catch(x=>console.log("catch",x)))
+    post(url, parameter) {
         return axios.post(host+url,parameter)
         .then(x=>{
             console.log("操作成功",x);
@@ -15,7 +14,43 @@ const httpcleint={
             console.log("操作失败",x);
             return x;
         });
-    }
+
+        // return new Promise((ok,notok)=>{
+        //     let value1,value2;
+        //     axios.post(host+url,parameter)
+        //     .then(x=>{
+        //         value1=x.data.result;
+        //         value1&&value2&&ok({value1,value2});
+        //     });
+        //     axios.post(host+url,parameter)
+        //     .then(x=>{
+        //         value2=x.data.result;
+        //         value1&&value2&&ok({value1,value2});
+        //     });
+        // })
+
+        // return new Promise((ok, notok) => {
+        //     axios.post(host + url, parameter)
+        //         .then(x => {
+        //             console.log("操作成功", x);
+        //             //return x.data.result;
+        //             ok(x.data.result)
+        //         })
+        //         .catch(x => {
+        //             console.log("操作失败", x);
+        //             //return x;
+        //             notok(x);
+        //         });
+        // });
+    },
+}
+
+export const _request = (url, parameter) => {
+    return httpcleint.post(url, parameter)
+        .then(data => data)
+        .catch(err => {
+            console.error(err)
+        })
 }
 
 
