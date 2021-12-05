07e2bd22af30cc9473b350e953ae543879f6e9bb
邓常青 <eedroom@163.com>
2021-12-05 18:07:30 +0800

    axio增加全局过滤器

--- api/apiinvoker.js
+++ api/apiinvoker.js
@@ -1,15 +1,19 @@
 import axios from 'axios'
 const host = "http://localhost:8084"
 import { message } from 'antd';
+let token="";
+axios.interceptors.request.use(function(cfg){
+    cfg.headers["Authorization"]=token;
+});
+axios.interceptors.response.use(cfg=>{
+    token=token||cfg.headers["Authorization"];
+})
 function    apiinvoker(a,b,c){
     let url=`${host}/${a.constructor.name}/${b}`;
-    let token="";
     return function (parameter) {
        return axios.post(url,parameter)
         .then(x=>{
             console.log("apiinvoker操作成功",x);
-            token=x.headers["Authorization"];
-            console.log("token",token);
             if(x.data.code!=200){
                 message.error(x.data.message||"程序内部错误！请稍后再试！");
                 return;
